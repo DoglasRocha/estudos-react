6aafc1c78751fa6e3b9529b596f81f9de7c59ff0
DoglasRocha <doglasrocha@alunos.utfpr.edu.br>
2023-02-14 17:20:14 -0300

    fim da aula 4

--- app02/src/App.js
+++ app02/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./components/img/ferrari-logo.png";
 
 function App() {
+  const nome = "Doglas";
+  const curso = "Curso de React";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main>
+      <header>
+        <p>{"Nome: " + nome}</p>
+        <br />
+        <p>{curso}</p>
       </header>
-    </div>
+      <section>
+        <img src={logo}></img>
+        <img src="img/ferrari-car.jpg" />
+      </section>
+    </main>
   );
 }
 
