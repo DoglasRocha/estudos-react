22143c711690d3df4366f4a0081c0b8b527b23cb
DoglasRocha <doglasrocha@alunos.utfpr.edu.br>
2023-02-20 15:00:54 -0300

    criando componentes: aula 6

---
+++ criando-componentes/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Data from "./components/Dados";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Data canal="Doglas Rocha" youtube="" curso="" />
+    </>
+  );
+}
+
+export default App;
---
+++ criando-componentes/src/components/Body.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Body() {
+  return (
+    <main>
+      <h2>Curso de React</h2>
+      <p>Boa tarde</p>
+    </main>
+  );
+}
