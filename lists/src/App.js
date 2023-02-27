e383d184510ff247dd1e444422752350896aecfa
DoglasRocha <doglasrocha@alunos.utfpr.edu.br>
2023-02-27 13:17:06 -0300

    utilizar forms

--- lists/src/App.js
+++ lists/src/App.js
@@ -1,13 +1,32 @@
-import React from "react";
-
-const carros = ["Onix", "Polo", "Ka", "Mobi", "Kwid", "Focus"];
+import React, { useState } from "react";
 
 function App() {
-  const listaCarros = carros.map((carro, index) => {
-    return <li key={index}>{carro}</li>;
-  });
+  const [name, setName] = useState("");
+  const [car, setCar] = useState("Onix");
 
-  return <ul>{listaCarros}</ul>;
+  const handlerOnChange = (component, setter) => {
+    setter(component.target.value);
+  };
+
+  return (
+    <>
+      <label>Digite seu nome: </label>
+      <input
+        type="text"
+        name="name"
+        onChange={(e) => handlerOnChange(e, setName)}
+        value={name}
+      />
+      <p>Nome digitado: {name}</p>
+      <select value={car} onChange={(e) => handlerOnChange(e, setCar)}>
+        <option value="Onix">Onix</option>
+        <option value="Polo">Polo</option>
+        <option value="Kwid">Kwid</option>
+        <option value="Focus">Onix</option>
+      </select>
+      <p>Carro selecionado: {car}</p>
+    </>
+  );
 }
 
 export default App;
